240e4ba39d37d3237e14640e4e6b8594c7489100
beavercoffee <beavercoffee@vidsoe.com>
2021-07-13 13:37:53 -0600

    BC

--- bc-cf7-fields.php
+++ bc-cf7-fields.php
@@ -12,12 +12,11 @@ Plugin URI: https://github.com/beavercoffee/bc-cf7-fields
 Requires at least: 5.7
 Requires PHP: 5.6
 Text Domain: bc-cf7-fields
-Version: 1.7.13
+Version: 1.7.13.1
 */
 
 if(defined('ABSPATH')){
     define('BC_CF7_FIELDS', __FILE__);
     require_once(plugin_dir_path(BC_CF7_FIELDS) . 'classes/class-bc-cf7-fields.php');
-    require_once(plugin_dir_path(BC_CF7_FIELDS) . 'classes/class-bc-fields.php');
     BC_CF7_Fields::get_instance(BC_CF7_FIELDS);
 }
