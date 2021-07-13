89fab8f26dbe2159215045ff95c4b3afd22c993e
vidsoe <labs@vidsoe.com>
2021-07-13 13:32:09 -0600

    BC

---
+++ bc-cf7-fields.php
@@ -0,0 +1,23 @@
+<?php
+/*
+Author: Beaver Coffee
+Author URI: https://beaver.coffee
+Description: Filter Contact Form 7 fields.
+Domain Path:
+License: GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Network: true
+Plugin Name: BC CF7 Fields
+Plugin URI: https://github.com/beavercoffee/bc-cf7-fields
+Requires at least: 5.7
+Requires PHP: 5.6
+Text Domain: bc-cf7-fields
+Version: 1.7.13
+*/
+
+if(defined('ABSPATH')){
+    define('BC_CF7_FIELDS', __FILE__);
+    require_once(plugin_dir_path(BC_CF7_FIELDS) . 'classes/class-bc-cf7-fields.php');
+    require_once(plugin_dir_path(BC_CF7_FIELDS) . 'classes/class-bc-fields.php');
+    BC_CF7_Fields::get_instance(BC_CF7_FIELDS);
+}
---
+++ classes/class-bc-cf7-fields.php
@@ -0,0 +1,212 @@
+<?php
+
+if(!class_exists('BC_CF7_Fields')){
+    final class BC_CF7_Fields {
+
+    	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    	//
+    	// private static
+    	//
+    	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+        private static $instance = null;
+
+    	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    	//
+    	// public static
+    	//
+    	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+        public static function get_instance($file = ''){
+            if(null !== self::$instance){
+                return self::$instance;
+            }
+            if('' === $file){
+                wp_die(__('File doesn&#8217;t exist?'));
+            }
+            if(!is_file($file)){
+                wp_die(sprintf(__('File &#8220;%s&#8221; doesn&#8217;t exist?'), $file));
+            }
+            self::$instance = new self($file);
+            return self::$instance;
+    	}
+
+    	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    	//
+    	// private
+    	//
+    	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+        private $file = '';
+
+    	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    	private function __clone(){}
+
+    	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    	private function __construct($file = ''){
+            $this->file = $file;
+            add_action('plugins_loaded', [$this, 'plugins_loaded']);
+        }
+
+    	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    	//
+    	// public
+    	//
+    	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+        public function plugins_loaded(){
+            if(!defined('BC_FUNCTIONS')){
+        		return;
+        	}
+            if(!defined('WPCF7_VERSION')){
+        		return;
+        	}
+            add_action('wpcf7_init', [$this, 'wpcf7_init']);
+            add_filter('wpcf7_validate_password', [$this, 'wpcf7_password_validation_filter'], 10, 2);
+            add_filter('wpcf7_validate_password*', [$this, 'wpcf7_password_validation_filter'], 10, 2);
+            add_filter('wpcf7_validate_radio*', 'wpcf7_checkbox_validation_filter', 10, 2);
+            remove_action('wpcf7_init', 'wpcf7_add_form_tag_acceptance');
+    		remove_action('wpcf7_init', 'wpcf7_add_form_tag_checkbox');
+    		remove_action('wpcf7_init', 'wpcf7_add_form_tag_date');
+    		remove_action('wpcf7_init', 'wpcf7_add_form_tag_file');
+    		remove_action('wpcf7_init', 'wpcf7_add_form_tag_number');
+    		remove_action('wpcf7_init', 'wpcf7_add_form_tag_select');
+            remove_action('wpcf7_init', 'wpcf7_add_form_tag_submit');
+    		remove_action('wpcf7_init', 'wpcf7_add_form_tag_text');
+    		remove_action('wpcf7_init', 'wpcf7_add_form_tag_textarea');
+            bc_build_update_checker('https://github.com/beavercoffee/bc-cf7-fields', $this->file, 'bc-cf7-fields');
+        }
+
+        // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+        public function wpcf7_init(){
+            wpcf7_add_form_tag('acceptance', function($tag){
+                $html = wpcf7_acceptance_form_tag_handler($tag);
+                $html = apply_filters('bc_cf7_field', $html, $tag, 'acceptance', 'acceptance');
+                return $html;
+            }, [
+        		'name-attr' => true,
+			]);
+			wpcf7_add_form_tag(['checkbox', 'checkbox*'], function($tag){
+                $html = wpcf7_checkbox_form_tag_handler($tag);
+                $html = apply_filters('bc_cf7_field', $html, $tag, 'checkbox', 'checkbox');
+                return $html;
+            }, [
+				'multiple-controls-container' => true,
+        		'name-attr' => true,
+                'selectable-values' => true,
+        	]);
+			wpcf7_add_form_tag(['date', 'date*'], function($tag){
+                $html = wpcf7_date_form_tag_handler($tag);
+                $html = apply_filters('bc_cf7_field', $html, $tag, 'date', 'date');
+                return $html;
+            }, [
+        		'name-attr' => true,
+        	]);
+            wpcf7_add_form_tag(['email', 'email*'], function($tag){
+                $html = wpcf7_text_form_tag_handler($tag);
+                $html = apply_filters('bc_cf7_field', $html, $tag, 'email', 'text');
+                return $html;
+            }, [
+        		'name-attr' => true,
+        	]);
+			wpcf7_add_form_tag(['file', 'file*'], function($tag){
+                $html = wpcf7_file_form_tag_handler($tag);
+                $html = apply_filters('bc_cf7_field', $html, $tag, 'file', 'file');
+                return $html;
+            }, [
+				'file-uploading' => true,
+        		'name-attr' => true,
+        	]);
+			wpcf7_add_form_tag(['number', 'number*'], function($tag){
+                $html = wpcf7_number_form_tag_handler($tag);
+                $html = apply_filters('bc_cf7_field', $html, $tag, 'number', 'number');
+                return $html;
+            }, [
+        		'name-attr' => true,
+        	]);
+            wpcf7_add_form_tag(['radio', 'radio*'], function($tag){
+                $html = wpcf7_checkbox_form_tag_handler($tag);
+                $html = apply_filters('bc_cf7_field', $html, $tag, 'radio', 'checkbox');
+                return $html;
+            }, [
+				'multiple-controls-container' => true,
+        		'name-attr' => true,
+                'selectable-values' => true,
+        	]);
+			wpcf7_add_form_tag(['range', 'range*'], function($tag){
+				$html = wpcf7_number_form_tag_handler($tag);
+                $html = apply_filters('bc_cf7_field', $html, $tag, 'range', 'number');
+                return $html;
+            }, [
+        		'name-attr' => true,
+        	]);
+			wpcf7_add_form_tag(['select', 'select*'], function($tag){
+                $html = wpcf7_select_form_tag_handler($tag);
+                $html = apply_filters('bc_cf7_field', $html, $tag, 'select', 'select');
+                return $html;
+            }, [
+        		'name-attr' => true,
+                'selectable-values' => true,
+        	]);
+            wpcf7_add_form_tag(['submit'], function($tag){
+                $html = wpcf7_submit_form_tag_handler($tag);
+                $html = apply_filters('bc_cf7_field', $html, $tag, 'submit', 'submit');
+                return $html;
+            });
+            wpcf7_add_form_tag(['password', 'password*'], function($tag){
+                $html = wpcf7_text_form_tag_handler($tag);
+                $html = apply_filters('bc_cf7_field', $html, $tag, 'password', 'text');
+                return $html;
+            }, [
+        		'name-attr' => true,
+        	]);
+            wpcf7_add_form_tag(['tel', 'tel*'], function($tag){
+                $html = wpcf7_text_form_tag_handler($tag);
+                $html = apply_filters('bc_cf7_field', $html, $tag, 'tel', 'text');
+                return $html;
+            }, [
+        		'name-attr' => true,
+        	]);
+            wpcf7_add_form_tag(['text', 'text*'], function($tag){
+                $html = wpcf7_text_form_tag_handler($tag);
+                $html = apply_filters('bc_cf7_field', $html, $tag, 'text', 'text');
+                return $html;
+            }, [
+        		'name-attr' => true,
+        	]);
+            wpcf7_add_form_tag(['textarea', 'textarea*'], function($tag){
+                $html = wpcf7_textarea_form_tag_handler($tag);
+                $html = apply_filters('bc_cf7_field', $html, $tag, 'textarea', 'textarea');
+                return $html;
+            }, [
+        		'name-attr' => true,
+        	]);
+            wpcf7_add_form_tag(['url', 'url*'], function($tag){
+                $html = wpcf7_text_form_tag_handler($tag);
+                $html = apply_filters('bc_cf7_field', $html, $tag, 'url', 'text');
+                return $html;
+            }, [
+        		'name-attr' => true,
+        	]);
+        }
+
+        // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+        public function wpcf7_password_validation_filter($result, $tag){
+            $name = $tag->name;
+			$value = isset($_POST[$name]) ? trim(wp_unslash(strtr((string) $_POST[$name], "\n", " "))) : '';
+			if('password' === $tag->basetype){
+				if($tag->is_required() and '' === $value){
+					$result->invalidate($tag, wpcf7_get_message('invalid_required'));
+				}
+			}
+			return wpcf7_text_validation_filter($result, $tag);
+        }
+
+    	// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    }
+}
